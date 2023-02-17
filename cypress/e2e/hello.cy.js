96b1ca86e038b216fc45b5f2e37fbad461aa0d03
NishadParekh <121786144+NishadParekh@users.noreply.github.com>
2023-02-17 13:33:04 +0530

    Update hello.cy.js

--- cypress/e2e/hello.cy.js
+++ cypress/e2e/hello.cy.js
@@ -3,9 +3,7 @@ describe('JustGitHub', function () {
     it("DummyLogin",function ()  {
       
         cy.visit("https://www.stealmylogin.com/demo.html")
-    /*cy.get('#user-name').type('ishita.popat@ecosmob.com')
-    cy.get('#user-password').type('Test@123')
-    cy.get('.btn').click()*/
+    
     })
 
 })
